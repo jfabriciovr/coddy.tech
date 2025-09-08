6b4a9fbbbfe5e14118d1a4b7ca88d5dbc4486558
jfabriciovr <fabricio33@gmail.com>
2025-09-07 22:31:47 -0600

    Add countUniqueVowels function and documentation for iterating over strings

---
+++ section-2-logic-and-flow/2.iterate-over-strings.js
@@ -0,0 +1,23 @@
+/* Challenge 
+
+Create a function named countUniqueVowels that takes a string as an argument 
+and returns the number of unique vowels in the string (vowels are a, e, i, o, u). 
+The function should not be case-insensitive. (include alse the uppercased vowels)
+
+*/
+
+function countUniqueVowels(str) {
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let foundVowels = [];
+  for (let char of str) {
+    if (vowels.includes(char) && !foundVowels.includes(char)) {
+      foundVowels.push(char);
+    }
+  }
+
+  return foundVowels.length;
+}
+
+console.log(countUniqueVowels("Hello World"));
+
+// output: 2
